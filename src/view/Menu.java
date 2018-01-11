502d7737f624c22271d090d7529b9f3d53443232
buoc1701 <32297420+buoc1701@users.noreply.github.com>
2018-01-11 10:30:00 +0100

    Structure as a start foundation

---
+++ src/view/Menu.java
@@ -0,0 +1,19 @@
+package view;
+
+public class Menu {
+
+    /* Concept for terminal view were we use variables like booleans to control phases
+     *  and object references to try method involved in each phase route or functionality.
+     *  Inputs and outputs, while loops with switch cases for robust interaction for user
+     *  were we also consider the way movement gets impacted.
+     *  Graphical design concludes and will be developed as we go.*/
+
+
+    private boolean menuFirstPhase;
+
+    public Menu() {
+    }
+
+
+
+}
