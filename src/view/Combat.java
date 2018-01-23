5e93321e473831681e8675cfaa43dd64293f217a
Axelhagnell <32957679+Axelhagnell@users.noreply.github.com>
2018-01-23 21:25:26 +0100

    Testing final phase of fights.

--- src/view/Combat.java
+++ src/view/Combat.java
@@ -6,30 +6,76 @@ import model.Character.Character;
 import model.Room;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Combat {
 
-    private boolean flee =false;
+    private boolean flee = false;
+    private Monster monster1;
+    private Monster monster2;
+    private Monster monster3;
+    private Monster monster4;
 
-    private int currentTurn;
+    private boolean monster1Alive = false;
+    private boolean monster2Alive = false;
+    private boolean monster3Alive = false;
+    private boolean monster4Alive = false;
+
     private int counter = 0;
+    private int monsterAlive = 0;
 
-    public void combatStart(Monster monster, Character character, Room rooms) throws IOException, InterruptedException {
+    public void combatStart(ArrayList<Monster> monster, Character character, Room rooms) throws IOException, InterruptedException {
         boolean cont = false;
         boolean loop = true;
         Scanner scan = new Scanner(System.in);
         int playerInitiativeRoll = 0;
-        int monsterInitiativeRoll = 0;
+        int monsterInitiativeRoll1 = 0;
+        int monsterInitiativeRoll2 = 0;
+        int monsterInitiativeRoll3 = 0;
+        int monsterInitiativeRoll4 = 0;
         clearWindow();
         rooms.printRoom();
 
-        System.out.println("a " + monster.getClassType() + " has appeared!");
+        try {
+            monster1 = monster.get(0);
+            monsterAlive++;
+            monster1Alive = true;
+            monster2 = monster.get(1);
+            monsterAlive++;
+            monster2Alive = true;
+            monster3 = monster.get(2);
+            monsterAlive++;
+            monster3Alive = true;
+            monster4 = monster.get(3);
+            monster4Alive = true;
+            monsterAlive++;
 
+        } catch (IndexOutOfBoundsException e) {
 
-        while(loop) {
+        }
+
+        while (loop) {
+            String encounterString = "";
+
+            for (int i = 0; i < monster.size();i++){
+                if (i+1 == monster.size() && monster.size() != 1){
+                    encounterString += "and a " + monster.get(i).getClassType() + " has appeared!";
+                    break;
+                }
+                if(monster.size()==1){
+                    encounterString += "a " + monster.get(i).getClassType() + " has appeared";
+                    break;
+                } else{
+                    encounterString += monster.get(i).getClassType() + ", ";
+                }
+            }
+
+            System.out.println(encounterString);
+            Thread.sleep(500);
             System.out.println("Do you want to (A)ttack it or try to (F)lee ?  ");
             System.out.println(character.getHp() + " hp left");
             String choice = scan.nextLine().toUpperCase();
@@ -40,9 +86,9 @@ public class Combat {
                 boolean tryToFlee;
                 tryToFlee = character.flee();
                 if (tryToFlee) {
-                    System.out.println("You fled from " + monster.getClassType());
+                    System.out.println("You've fled! ");
                     rooms.flee();
-                    flee=true;
+                    flee = true;
                     cont = false;
                     loop = false;
                 } else {
@@ -50,8 +96,7 @@ public class Combat {
                     cont = true;
                     loop = false;
                 }
-            }
-            else {
+            } else {
                 System.out.println("You need to choose between (A)ttack or (F)lee ");
             }
         }
@@ -59,41 +104,80 @@ public class Combat {
             character.setFirstHit(true);
 
             //rollar initiative för att se om monstret eller spelaren ska börja
-            for (int i = 1; i <= monster.getInitiative(); i++) {
-                monsterInitiativeRoll = ThreadLocalRandom.current().nextInt(1, 6 + 1) + monsterInitiativeRoll;
+
+            try {
+                for (int i = 1; i <= monster.get(0).getInitiative(); i++) {
+                    monsterInitiativeRoll1 = ThreadLocalRandom.current().nextInt(1, 6 + 1) + monsterInitiativeRoll1;
+                }
+
+                for (int i = 1; i <= monster.get(1).getInitiative(); i++) {
+                    monsterInitiativeRoll2 = ThreadLocalRandom.current().nextInt(1, 6 + 1) + monsterInitiativeRoll2;
+                }
+
+                for (int i = 1; i <= monster.get(2).getInitiative(); i++) {
+                    monsterInitiativeRoll3 = ThreadLocalRandom.current().nextInt(1, 6 + 1) + monsterInitiativeRoll3;
+                }
+
+                for (int i = 1; i <= monster.get(3).getInitiative(); i++) {
+                    monsterInitiativeRoll4 = ThreadLocalRandom.current().nextInt(1, 6 + 1) + monsterInitiativeRoll4;
+                }
+
+            } catch (IndexOutOfBoundsException e1) {
+
             }
+
 
             for (int i = 1; i <= character.getInitiative(); i++) {
                 playerInitiativeRoll = ThreadLocalRandom.current().nextInt(1, 6 + 1) + playerInitiativeRoll;
+
             }
             System.out.println("------------------------------");
             System.out.println("You rolled... " + playerInitiativeRoll);
-            System.out.println(monster.getClassType() + " rolled... " + monsterInitiativeRoll);
+            try {
+                System.out.println(monster.get(0).getClassType() + " rolled... " + monsterInitiativeRoll1);
+                System.out.println(monster.get(1).getClassType() + " rolled... " + monsterInitiativeRoll2);
+                System.out.println(monster.get(2).getClassType() + " rolled... " + monsterInitiativeRoll3);
+                System.out.println(monster.get(3).getClassType() + " rolled... " + monsterInitiativeRoll4);
+            } catch (IndexOutOfBoundsException e2) {
+
+            }
             System.out.println("------------------------------");
             System.out.println("");
+
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            //jämför rollsen och ger current turn till den som rollade högst (currentTurn 1 spelare, 0 monster)
-            if (playerInitiativeRoll > monsterInitiativeRoll) {
-                System.out.println("You go first! ");
-                this.currentTurn = 1;
-            } else {
-                System.out.println(monster.getClassType() + " goes first! ");
-                this.currentTurn = 0;
+
+            ArrayList<Integer> listOfRolls = new ArrayList<Integer>();
+            listOfRolls.add(playerInitiativeRoll);
+
+            if (monsterAlive >= 1) {
+                listOfRolls.add(monsterInitiativeRoll1);
             }
+            if (monsterAlive >= 2) {
+                listOfRolls.add(monsterInitiativeRoll2);
+            }
+            if (monsterAlive >= 3) {
+                listOfRolls.add(monsterInitiativeRoll3);
+            }
+            if (monsterAlive == 4) {
+                listOfRolls.add(monsterInitiativeRoll4);
+            }
+
+
+            Collections.sort(listOfRolls);
+            Collections.reverse(listOfRolls);
 
             //Combat startas
             while (true) {
 
-                if (counter == 2){
+                if (counter == 2) {
                     try {
                         Thread.sleep(2000);
                         counter = 0;
-                    }
-                    catch(InterruptedException e){
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
 
@@ -106,62 +190,160 @@ public class Combat {
                 }
 
                 //kollar om monstret dog förra rundan
-                if (monster.getHp() <= 0) {
-                    System.out.println(monster.getClassType() + " died!");
-                    try {
-                        Thread.sleep(2500);
-                    }
-                    catch(InterruptedException e){
-                        e.printStackTrace();
-                    }
+
+                try {
+                    Thread.sleep(2500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                String whoAttacked = "";
+
+                if (monsterAlive == 0) {
+                    System.out.println("Every monster died ");
                     break;
+
                 }
+                for (int i = 0; i < listOfRolls.size(); i++) {
 
-                if (this.currentTurn == 1) {
-                    //frågar spelaren vad hen vill göra och spar det till en variabel
-                    System.out.println("What do you want to do? ");
-                    System.out.println("(A)ttack, (F)lee");
-                    String playerChoice = scan.nextLine().toUpperCase();
-
-                    if (playerChoice.equals("A")) {
-                        character.attackMonster(monster, false);
-                        this.currentTurn = 0;
-                    } else if (playerChoice.equals("F")) {
-
-                        boolean flee = character.flee();
-
-                        if (flee) {
-                            System.out.println("You fled! ");
-                            rooms.flee();
-                            break;
-                        } else {
-                            System.out.println("You've failed your escape. ");
-                            this.currentTurn = 0;
+                    try {
+                        if (monster1.getHp() <= 0 && monster1Alive) {
+                            System.out.println(monster.get(0).getClassType() + " died!");
+                            monster1Alive = false;
+                            monsterAlive--;
                         }
+                        if (monster2.getHp() <= 0 && monster2Alive) {
+                            System.out.println(monster.get(1).getClassType() + " died!");
+                            monster2Alive = false;
+                            monsterAlive--;
+                        }
+                        if (monster3.getHp() <= 0&& monster3Alive) {
+                            System.out.println(monster.get(2).getClassType() + " died!");
+                            monster3Alive = false;
+                            monsterAlive--;
+                        }
+                        if (monster4.getHp() <= 0&&monster4Alive) {
+                            System.out.println(monster.get(3).getClassType() + " died!");
+                            monster4Alive = false;
+                            monsterAlive--;
+                        }
+                    } catch (NullPointerException e) {
 
-
-                    } else {
-                        System.out.println("You need to (A)ttack or try to (F)lee");
                     }
-                } else if (this.currentTurn == 0) {
-                    character.defendAttack(monster,false);
-                    this.currentTurn = 1;
+
+                    if (playerInitiativeRoll == listOfRolls.get(i) && !"player".equals(whoAttacked)) {
+                        //frågar spelaren vad hen vill göra och spar det till en variabel
+                        System.out.println("What do you want to do? ");
+                        System.out.println("(A)ttack, (F)lee");
+                        String playerChoice = scan.nextLine().toUpperCase();
+                        int commandToAttack = 1;
+
+                        if (playerChoice.equals("A")) {
+                            System.out.println("Who do you want to attack? ");
+                            for (int n = 0; n < monster.size(); n++) {
+                                if(monster.get(n).getHp()<=0){
+                                    System.out.println(commandToAttack + ". " + monster.get(n).getClassType() + " (dead)");
+                                    commandToAttack++;
+                                }   else {
+                                    System.out.println(commandToAttack + ". " + monster.get(n).getClassType());
+                                    commandToAttack++;
+                                }
+                            }
+
+                            while (true) {
+                                String target = scan.nextLine();
+
+                                if (target.equals("1")) {
+                                    if(monster1Alive) {
+                                        character.attackMonster(monster1, false);
+                                        break;
+                                    }
+                                    else{
+                                        System.out.println("You cant attack a dead monster");
+                                        Thread.sleep(1000);
+                                    }
+                                }
+
+                                else if (target.equals("2")) {
+                                    if(monster2Alive) {
+                                        character.attackMonster(monster2, false);
+                                        break;
+                                    }
+                                    else{
+                                        System.out.println("You cant attack a dead monster");
+                                        Thread.sleep(1000);
+                                    }
+
+                                }
+
+                                else if (target.equals("3")) {
+                                    if(monster3Alive) {
+                                        character.attackMonster(monster3, false);
+                                        break;
+                                    }
+                                    else{
+                                        System.out.println("You cant attack a dead monster");
+                                        Thread.sleep(1000);
+                                    }
+                                }
+
+                                else if (target.equals("4")) {
+                                    if(monster4Alive){
+                                        character.attackMonster(monster4, false);
+                                        break;
+                                    }
+                                    else{
+                                        System.out.println("You cant attack a dead monster");
+                                        Thread.sleep(1000);
+                                    }
+                                }
+                            }
+
+                        } else if (playerChoice.equals("F")) {
+
+                            boolean flee = character.flee();
+
+                            if (flee) {
+                                System.out.println("You fled! ");
+                                rooms.flee();
+
+                            } else {
+                                System.out.println("You've failed your escape. ");
+
+                            }
+
+
+                        } else {
+                            System.out.println("You need to (A)ttack or try to (F)lee");
+                        }
+                        whoAttacked = "player";
+
+                    } else if (monster1Alive && monsterInitiativeRoll1 == listOfRolls.get(i) && !"monster1".equals(whoAttacked)) {
+                        character.defendAttack(monster1, false);
+                        whoAttacked = "monster1";
+
+                    } else if (monster2Alive && monsterInitiativeRoll2 == listOfRolls.get(i)&& !"monster2".equals(whoAttacked)) {
+                        character.defendAttack(monster2, false);
+                        whoAttacked = "monster2";
+
+                    } else if (monster3Alive && monsterInitiativeRoll3 == listOfRolls.get(i)&& !"monster3".equals(whoAttacked)) {
+                        character.defendAttack(monster3, false);
+                        whoAttacked = "monster3";
+
+                    } else if (monster4Alive && monsterInitiativeRoll4 == listOfRolls.get(i)&& !"monster4".equals(whoAttacked)) {
+                        character.defendAttack(monster4, false);
+                        whoAttacked = "monster4";
+                    }
+                    this.counter++;
                 }
-                this.counter++;
+
             }
+        }
 
         }
-    }
+
+
     public void clearWindow() throws IOException, InterruptedException {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-    }
-
-    public int getCurrentTurn() {
-        return currentTurn;
-    }
-
-    public void setCurrentTurn(int currentTurn) {
-        this.currentTurn = currentTurn;
     }
 
     @Override
@@ -169,13 +351,14 @@ public class Combat {
         if (this == o) return true;
         if (!(o instanceof Combat)) return false;
         Combat combat = (Combat) o;
-        return getCurrentTurn() == combat.getCurrentTurn();
+        return true;
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(getCurrentTurn());
+       // return Objects.hash(getCurrentTurn());
+        return 1;
     }
 
     public boolean getFlee() {
@@ -186,3 +369,4 @@ public class Combat {
         this.flee = flee;
     }
 }
+
